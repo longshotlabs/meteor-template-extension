69de6ba051cce0b6b47d434b842db33135c46965
aaronjudd <aaronjudd@me.com>
2014-09-05 11:14:53 -0700

    Update package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "aldeed:template-extension",
   summary: "Replace already defined templates, inherit helpers and events from other templates",
-  version: "1.0.0",
+  version: "1.0.1",
   git: "https://github.com/aldeed/meteor-template-extension.git"
 });
 
