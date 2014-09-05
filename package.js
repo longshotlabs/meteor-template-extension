d8897aa47f55ae6814cafcf15e18e42e29b87294
Eric Dobbertin <eric@dairystatedesigns.com>
2014-09-05 07:32:46 -0500

    Meteor 0.9.1 support

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: "aldeed:template-extension",
   summary: "Replace already defined templates, inherit helpers and events from other templates",
-  version: "0.1.0",
+  version: "1.0.0",
   git: "https://github.com/aldeed/meteor-template-extension.git"
 });
 
 Package.on_use(function(api) {
   if (api.versionsFrom) {
-    api.versionsFrom('METEOR@0.9.0');
+    api.versionsFrom('METEOR@0.9.1');
   }
-  api.use('templating');
+  api.use(['templating', 'blaze']);
 
   api.add_files(['template-extension.js'], 'client');
 });
