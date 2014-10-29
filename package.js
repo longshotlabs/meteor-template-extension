831a450139769fb5f2a70b69a8bece04e854940f
aaronjudd <aaronjudd@me.com>
2014-10-29 08:29:54 -0700

    Add jquery dependency

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   name: "aldeed:template-extension",
   summary: "Adds template features currently missing from the templating package",
-  version: "3.1.0",
+  version: "3.1.1",
   git: "https://github.com/aldeed/meteor-template-extension.git"
 });
 
 Package.on_use(function(api) {
   api.use([
     'templating@1.0.0',
-    'blaze@2.0.0'
+    'blaze@2.0.0',
+    'jquery'
   ]);
 
   api.add_files(['template-extension.js'], 'client');
