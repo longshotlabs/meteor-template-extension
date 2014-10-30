899fe29a6f0b5a39f69b898d2e04c7e5f59f25b1
Eric Dobbertin <eric@dairystatedesigns.com>
2014-10-30 14:52:48 -0500

    add jquery version

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.on_use(function(api) {
   api.use([
     'templating@1.0.0',
     'blaze@2.0.0',
-    'jquery'
+    'jquery@1.0.0'
   ]);
 
   api.add_files(['template-extension.js'], 'client');
