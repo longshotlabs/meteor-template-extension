7d4aec65b9f107d1305cc72cd34261ef24a71b71
Eric Dobbertin <eric@dairystatedesigns.com>
2015-02-08 22:04:54 -0600

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "aldeed:template-extension",
   summary: "Adds template features currently missing from the templating package",
-  version: "3.2.0",
+  version: "3.3.0",
   git: "https://github.com/aldeed/meteor-template-extension.git"
 });
 
